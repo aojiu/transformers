70299dc7128a543fa8df8ab27a37318f3c09febe
aojiu <xieweiyao@gmail.com>
2020-03-07 17:40:20 -0500

    add the boolqprocessor in glue.py

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -17,7 +17,7 @@
 
 import logging
 import os
-
+import jsonlines
 from ...file_utils import is_tf_available
 from .utils import DataProcessor, InputExample, InputFeatures
 
@@ -515,6 +515,47 @@ class WnliProcessor(DataProcessor):
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
+# Step 2a
+# Implement a BoolqProcessor
+class BoolqProcessor(DataProcessor):
+    """Processor for the BoolQ data set (GLUE version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+            """See base class."""
+            return InputExample(
+                tensor_dict["idx"].numpy(),
+                tensor_dict["question"].numpy().decode("utf-8"),
+                tensor_dict["passage"].numpy().decode("utf-8"),
+                str(tensor_dict["label"].numpy()),
+            )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(os.path.join(data_dir, "train.jsonl"), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(os.path.join(data_dir, "val.jsonl"), "val")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, filename, set_type):
+        """Creates examples for the training and dev sets."""
+
+        examples = []
+        # read the jsonline file as dictionary
+        # get question, passage, and label
+        with jsonlines.open('BoolQ/val.jsonl') as reader:
+            for obj in reader:
+                question = obj["question"]
+                passage = obj["passage"]
+                label = obj["label"]
+                guid = "%s-%s" % (set_type, obj["idx"])
+                examples.append(InputExample(guid=guid, question=text_a, passage=text_b, label=label))
+        return examples
+
 
 glue_tasks_num_labels = {
     "cola": 2,
@@ -526,6 +567,7 @@ glue_tasks_num_labels = {
     "qnli": 2,
     "rte": 2,
     "wnli": 2,
+    "boolq": 2
 }
 
 glue_processors = {
@@ -539,6 +581,7 @@ glue_processors = {
     "qnli": QnliProcessor,
     "rte": RteProcessor,
     "wnli": WnliProcessor,
+    "boolq": BoolqProcessor
 }
 
 glue_output_modes = {
@@ -552,4 +595,5 @@ glue_output_modes = {
     "qnli": "classification",
     "rte": "classification",
     "wnli": "classification",
+    "boolq": "classification",
 }
